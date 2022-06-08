eb4cf8969e7ddf2c485e91a61716e1eb953e2636
jovinkendrico <jovinkendrico@gmail.com>
2022-06-08 22:01:52 +0700

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,73 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import transaksi.Nasabah;
+
+public class App {
+    public static void main(String[] args) throws Exception{
+        try (Scanner scanner = new Scanner(System.in)) {
+            ArrayList<Nasabah> arrayNasabah = new ArrayList<Nasabah>();
+            arrayNasabah.add(new Nasabah("Susi", 10000000, "0312456"));
+            arrayNasabah.add(new Nasabah("Budi", 1000000, "0314865"));
+            boolean cekNomorRekening = true;
+            do{
+                System.out.println("=====================");
+                System.out.println("Program Aplikasi Bank");
+                System.out.println("=====================");
+                System.out.print("Masukkan Nomor Rekening yang ingin digunakan = ");
+                String nomorRekening = scanner.next();
+                for(Nasabah nasabah : arrayNasabah){
+                    if(nasabah.getNomorRekening().equals(nomorRekening)){
+                        cekNomorRekening = false;
+                        boolean ulang = true;
+                        do{
+                            System.out.println("=============================================================================");
+                            System.out.println("Menu Aplikasi Bank");
+                            System.out.println("=============================================================================");
+                            System.out.println("1.Transfer Uang");
+                            System.out.println("2.Cek Mutasi");
+                            System.out.println("3.Keluar");
+                            System.out.println("=============================================================================");
+                            System.out.print("Masukkan pilihan anda (1-3) = ");
+                            int pilih = scanner.nextInt();
+                            if(pilih == 1){
+                                System.out.println("=============================================================================");
+                                System.out.print("Masukkan nomor rekening tujuan = ");
+                                String nomorRekeningTujuan = scanner.next();
+                                if(nasabah.checkNasabah(arrayNasabah, nomorRekeningTujuan)){
+                                    System.out.println("=============================================================================");
+                                    System.out.print("Masukkan jumah uang yang ingin ditransfer = ");
+                                    int jumlah = scanner.nextInt();
+                                    if(nasabah.saldoTersedia(jumlah)){
+                                        nasabah.transferUang(jumlah, nomorRekeningTujuan, arrayNasabah);
+                                    }else{
+                                        System.out.println("=============================================================================");
+                                        System.out.println("Saldo tidak mencukupi");
+                                    }
+                                }else{
+                                    System.out.println("=============================================================================");
+                                    System.out.println("Nomor Rekening tujuan tidak terdaftar di bank");
+                                    continue;
+                                }
+                            }else if(pilih == 2){
+                                nasabah.cetakMutasi();
+                            }
+                            else{
+                                ulang = false;
+                            }
+                        }while(ulang);
+                    }
+                }
+                clearScreen();
+            }while(cekNomorRekening);
+            
+            
+        }
+    }
+
+    public static void clearScreen(){
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
+    }
+}
---
+++ src/transaksi/Nasabah.java
@@ -0,0 +1,93 @@
+package transaksi;
+import java.util.ArrayList;
+
+public class Nasabah {
+    public String nama;
+    public int saldo;
+    public String nomorRekening;
+    ArrayList<String> logMutasi = new ArrayList<String>();
+   
+    public Nasabah(String nama, int saldo, String nomorRekening){
+        this.nama = nama;
+        this.saldo = saldo;
+        this.nomorRekening = nomorRekening;
+    }
+
+    public Nasabah(Nasabah nasabah) {
+    }
+
+    public boolean saldoTersedia(int jumlah){
+        if(this.saldo >= jumlah){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkNasabah(ArrayList<Nasabah> arrayNasabah, String nomorRekeningTujuan){
+        for(Nasabah nasabah : arrayNasabah){
+            if (nasabah.getNomorRekening().equals(nomorRekeningTujuan)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public void transferUang(int jumlah, String nomorRekeningTujuan, ArrayList<Nasabah> arrayNasabah){
+        tarikUang(jumlah);
+        terimaUang(jumlah, nomorRekeningTujuan,arrayNasabah);
+        logMutasi.add("Transfer uang sebesar "+ jumlah + " berhasil");
+        tambahMutasi(jumlah, nomorRekeningTujuan, arrayNasabah);
+        System.out.println("=============================================================================");
+        System.out.println("Transfer Berhasil");
+    }
+
+    private void tambahMutasi(int jumlah, String nomorRekeningTujuan, ArrayList<Nasabah> arrayNasabah){
+        for(Nasabah nasabah: arrayNasabah){
+            if(nasabah.getNomorRekening().equals(nomorRekeningTujuan)){
+                nasabah.logMutasi.add(this.getNama() + " mengirim uang sebesar " + jumlah + "ke rekening anda");
+            }
+        }
+    }
+
+    private void tarikUang(int jumlah){
+        this.saldo-=jumlah;
+    }
+
+    private void terimaUang(int jumlah, String nomorRekeningTujuan, ArrayList<Nasabah> arrayNasabah){
+        for(Nasabah nasabah: arrayNasabah){
+            if(nasabah.getNomorRekening().equals(nomorRekeningTujuan)){
+                nasabah.saldo+=jumlah;
+            }
+        }
+    }
+
+    public void cetakMutasi(){
+        for(String log : logMutasi){
+            System.out.println("=============================================================================");
+            System.out.println(log);
+        }
+    }
+
+    public String getNama() {
+        return this.nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getSaldo() {
+        return this.saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getNomorRekening() {
+        return this.nomorRekening;
+    }
+
+    public void setNomorRekening(String nomorRekening) {
+        this.nomorRekening = nomorRekening;
+    }
+}
